e677d7909c3edd2853cf95d0bfff3a7b3c6acb49
Paul Lewallen <paullewallencom@users.noreply.github.com>
2021-10-10 13:10:11 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+//
+
+function countDown(c) {
+    for (let i = c; i >= 0; i--) {
+        console.log(i);
+    }
+}
+
+function writeCards(n, m) {
+    let f = [];
+    for (let i = 0; i < n.length; i++) {
+        f.push("Thank you, " + n[i] + ", for the wonderful " + m + " gift!");
+    }
+    return f;
+}
